f4782721cfe2f337fc8b9ac4b6ed7e0814b99553
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-22 01:49:47 +0900

    [변경]:webpack.config.js 내용 변경

--- webpack.config.js
+++ webpack.config.js
@@ -2,156 +2,97 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const appIndex = path.resolve(__dirname, "src", "index.tsx");
-const appHtml = path.resolve(__dirname, "public", "index.html");
-const appBuild = path.resolve(__dirname, "build");
-const appPublic = path.resolve(__dirname, "public");
-
-function getClientEnv(nodeEnv) {
-  return {
-    "process.env": JSON.stringify(
-      Object.keys(process.env)
-        .filter((key) => /^REACT_APP/i.test(key))
-        .reduce(
-          (env, key) => {
-            env[key] = process.env[key];
-            return env;
-          },
-          {
-            NODE_ENV: nodeEnv,
-          },
-        ),
-    ),
-  };
-}
-
-module.exports = (webpackEnv) => {
+module.exports = () => {
   const isEnvDevelopment = process.env.NODE_ENV === "development";
   const isEnvProduction = process.env.NODE_ENV === "production";
-  const clientEnv = getClientEnv(webpackEnv);
 
   return {
     mode: isEnvDevelopment ? "development" : "production",
+    devtool: isEnvDevelopment ? "inline-source-map" : "hidden-source-map",
     resolve: {
       extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
       modules: [path.join(__dirname, "src"), "node_modules"],
       alias: {
-        "@hooks": path.resolve(__dirname, "src", "hooks"),
         "@components": path.resolve(__dirname, "src", "components"),
+        "@hooks": path.resolve(__dirname, "src", "hooks"),
         "@pages": path.resolve(__dirname, "src", "pages"),
-        "@utils": path.resolve(__dirname, "src", "utils"),
         "@static": path.resolve(__dirname, "src", "static"),
+        "@utils": path.resolve(__dirname, "src", "utils"),
       },
     },
-    devtool: isEnvDevelopment ? "inline-source-map" : "hidden-source-map",
     devServer: {
       port: 3000,
       historyApiFallback: true,
-      contentBase: appPublic,
-      open: true,
-      overlay: true,
+      contentBase: path.resolve(__dirname, "dist"), // 서버가 로딩할 static 파일 경로 지정
       stats: "errors-warnings",
+      overlay: true,
     },
-    cache: {
-      type: isEnvDevelopment ? "memory" : isEnvProduction && "filesystem",
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-      splitChunks: {
-        chunks: "all",
-      },
-    },
-    entry: appIndex,
+    entry: path.join(__dirname, "src", "index"),
     module: {
       rules: [
         {
-          oneOf: [
-            {
-              test: /\.jsx?$/,
-              use: [
-                "cache-loader",
-                {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
-                  },
-                },
-              ],
-              exclude: path.join(__dirname, "node_modules"),
+          test: /\.jsx?$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
-            {
-              test: /\.(ts|tsx)$/,
-              use: [
-                "cache-loader",
-                {
-                  loader: "ts-loader",
-                  options: {
-                    transpileOnly: !!isEnvDevelopment,
-                  },
-                },
-              ],
-              exclude: path.join(__dirname, "node_modules"),
+          },
+          exclude: path.join(__dirname, "node_modules"),
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: !isEnvDevelopment,
             },
-            {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: "url-loader",
-              options: {
-                limit: 10000,
-                outputPath: "static/media",
-                name: "[name].[hash:8].[ext]",
-              },
-            },
-            {
-              test: /\.(png|jpe?g|gif)$/i,
-              use: [
-                {
-                  loader: "file-loader",
-                  options: {
-                    name: "static/media/[name].[hash:8].[ext]",
-                  },
-                },
-              ],
-            },
-          ],
+          },
+          exclude: path.join(__dirname, "node_modules"),
+        },
+        {
+          loader: "url-loader",
+          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+          options: {
+            limit: 10000,
+            outputPath: "static/", // 파일이 저장될 시스템 경로 지정
+            name: "[name].[hash:8].[ext]",
+          },
+        },
+        {
+          loader: "file-loader",
+          exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+          options: {
+            outputPath: "static/", // 파일이 저장될 시스템 경로 지정
+            name: "[name].[hash:8].[ext]",
+          },
         },
       ],
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: appHtml }),
-      new webpack.DefinePlugin(clientEnv),
+      isEnvDevelopment && new ReactRefreshWebpackPlugin(),
+      isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
+      isEnvDevelopment && new BundleAnalyzerPlugin({ analyzerMode: "server", openAnalyzer: false }),
+      isEnvProduction && new BundleAnalyzerPlugin({ analyzerMode: "static" }),
+      isEnvProduction && new webpack.LoaderOptionsPlugin({ minimize: true }),
+      new webpack.EnvironmentPlugin({ NODE_ENV: isEnvDevelopment ? "development" : "production" }),
+      new HtmlWebpackPlugin({
+        inject: "body",
+        template: path.join(__dirname, "public", "/index.html"),
+      }),
       new ForkTsCheckerWebpackPlugin({
         eslint: {
           files: "./src/**/*.{ts,tsx,js,jsx}",
         },
       }),
-      new ManifestPlugin({
-        fileName: "asset-manifest.json",
-        publicPath: appPublic,
-        generate: (seed, files, entrypoints) => {
-          const manifestFiles = files.reduce((manifest, file) => {
-            manifest[file.name] = file.path;
-            return manifest;
-          }, seed);
-          const entrypointFiles = entrypoints.main.filter((fileName) => !fileName.endsWith(".map"));
-          return {
-            files: manifestFiles,
-            entrypoints: entrypointFiles,
-          };
-        },
-      }),
-      isEnvProduction && new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     output: {
-      path: appBuild,
-      filename: isEnvProduction ? "static/js/[name].[contenthash:8].js" : isEnvDevelopment && "static/js/bundle.js",
-      chunkFilename: isEnvProduction
-        ? "static/js/[name].[contenthash:8].chunk.js"
-        : isEnvDevelopment && "static/js/[name].chunk.js",
+      path: path.join(__dirname, "dist"), // 번들링 한 결과를 보관 할 디렉터리 위치
+      filename: "[name].[hash:8].js",
+      publicPath: "/dist/", // 모든 파일의 접두사
     },
   };
 };
